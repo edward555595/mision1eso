1dabb733e1cb9a6303d68f9d74989c2290f20e37
luisecol <150181963+edward555595@users.noreply.github.com>
2026-06-30 22:37:59 +0200

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,6 +1,5 @@
-// Configuración Firebase Mision1ESO
 window.M1ESO_FIREBASE_CONFIG = {
- apiKey: "AIzaSyDsygfFMLwriz6m0DG7JaQ9kPXnmdY-5-o",
+  apiKey: "AIzaSyDsygfFMLwriz6m0DG7JaQ9kPXnmdY-5-o",
   authDomain: "mision1eso.firebaseapp.com",
   projectId: "mision1eso",
   storageBucket: "mision1eso.firebasestorage.app",
@@ -8,7 +7,6 @@ window.M1ESO_FIREBASE_CONFIG = {
   appId: "1:218253528165:web:f1a5585c58209274f80a8c"
 };
 
-// Sustituir después por el UID real del profesor.
 window.M1ESO_ADMIN_UIDS = [
   "PEGA_AQUI_UID_PROFESOR"
 ];
